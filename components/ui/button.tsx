80ea787e215829802b98f5f5bca0c000113a48f1
Abdul Hai Rawan <abdulhairawan@gmail.com>
2024-06-12 19:13:46 +0430

    hero section & shoe card completed

---
+++ components/ui/button.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+interface ButtonProps {
+  label: string;
+  iconURL: string;
+}
+
+const Button = ({ label, iconURL }: ButtonProps) => {
+  return (
+    <button
+      className="flex justify-center items-center gap-2 px-7 py-4 border
+                       font-montserrat text-lg leading-none bg-coral-red rounded-full
+                       text-white border-coral-red"
+    >
+      {label}
+      <Image
+        src={iconURL}
+        alt={"button icon"}
+        className="ml-2 rounded-full"
+        height={20}
+        width={20}
+      />
+    </button>
+  );
+};
+
+export default Button;
