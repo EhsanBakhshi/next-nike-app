a9f4a41ea166381ffb6b19695b55b54483fe2179
Abdul Hai Rawan <abdulhairawan@gmail.com>
2024-06-17 14:46:28 +0430

    services-section finished

--- components/sections/services.tsx
+++ components/sections/services.tsx
@@ -1,5 +1,14 @@
+import ServicesCard from "@/components/services-card";
+import { services } from "@/constants";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service, index) => (
+        <ServicesCard key={index} data={service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,9 @@ export type Product = {
   name: string;
   price: string;
 };
+
+export type Service = {
+  imgURL: string;
+  label: string;
+  subtext: string;
+};
